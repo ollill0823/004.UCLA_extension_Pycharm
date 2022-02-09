f9541c052d619476237f716e7a27ae0f475fd4bf
ollill0823 <ppwang8823@gmail.com>
2022-02-08 23:49:59 -0800

    deleted

--- W004-1/Ex04.open txt file.py
+++
@@ -1,8 +0,0 @@
-with open ('i-have-a-dream.txt', 'r', encoding="utf-8") as f:
-    article=f.read()
-    split=article.split()
-
-print(split)
-
-
-
--- W004-1/Ex12.test.py
+++
@@ -1,9 +0,0 @@
-def string_splosion(str):
-  result = ""
-  # On each iteration, add the substring of the chars 0..i
-  for i in range(len(str)):
-    result = result + str[:i+1]
-  return result
-
-a='ababcabcd'
-print(string_splosion(a))
